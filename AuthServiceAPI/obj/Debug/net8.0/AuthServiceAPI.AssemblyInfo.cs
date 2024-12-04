1824bf3d5813de61e9bc38b40ed6b58a9b82929d
cbjoed <cbjoedstrup@gmail.com>
2024-12-04 12:05:31 +0100

    virker som den skal. måske skal der tilføjes mere logging

--- AuthServiceAPI/obj/Debug/net8.0/AuthServiceAPI.AssemblyInfo.cs
+++ AuthServiceAPI/obj/Debug/net8.0/AuthServiceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e1b05613233b01ade2c060f8cc846faefe12f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9bfad9293a21984bdbbd46d53b9c9499d1abd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
